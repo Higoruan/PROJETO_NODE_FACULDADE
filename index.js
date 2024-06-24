cfe699d5caf4bc1e76f09827101608ac6c290304
Higor <higorruann0510@gmail.com>
2024-06-23 22:44:17 -0300

    Venda finalizada

--- index.js
+++ index.js
@@ -29,9 +29,9 @@ app.get('/cliente', (req, res) => {
 
 app.post('/cliente', (req, res) => {
     let name = req.body.name;
-    let cpf = req.body.cpf;
+    let cpf = req.body.cpf.replace(/[^\d]/g, '');
     let address = req.body.address;
-    let phone = req.body.phone;
+    let phone = req.body.phone.replace(/[^\d]/g, '');
     const query = 'INSERT INTO customer (name, cpf, address, phone) VALUES (?, ?, ?, ?)';
     connection.query(query, [name, cpf, address, phone], (err, results) => {
         if (err) {
@@ -43,16 +43,214 @@ app.post('/cliente', (req, res) => {
     });
 });
 
+app.get('/cliente/edit/:id', (req, res) => {
+    console.log(req.params);
+    let id = req.params.id;
+    const query = "SELECT * FROM customer WHERE id = ?";
+    connection.query(query, [id], (err, results) => {
+        if (err) {
+            console.error('Erro ao editar dados:', err);
+            res.status(500).send('Erro ao editar dados no banco de dados.');
+            return;
+        }
+
+        res.render('ClienteEdit', { customer: results[0] });
+    });
+})
+
+app.post('/cliente/update/:id', (req, res) => {
+    let id = req.params.id;
+    const { name, cpf, address, phone } = req.body;
+    const cleanCpf = cpf.replace(/[^\d]/g, '');
+    const cleanPhone = phone.replace(/[^\d]/g, '');
+
+    const query = "UPDATE customer SET name = ?, cpf = ?, address = ?, phone = ? WHERE id = ?";
+
+    connection.query(query, [name, cleanCpf, address, cleanPhone, id], (err, results) => {
+        if (err) {
+            console.error('Erro ao atualizar dados:', err);
+            res.status(500).send('Erro ao atualizar dados no banco de dados.');
+            return;
+        }
+
+        res.redirect('/Cliente');
+    });
+});
+
+app.post('/cliente/delete/:id', (req, res) => {
+    let id = req.params.id;
+    const query = "DELETE FROM customer WHERE id = ?";
+    connection.query(query, [id], (err, results) => {
+        if (err) {
+            console.error('Erro ao deletar dados:', err);
+            res.status(500).send('Erro ao deletar dados no banco de dados.');
+            return;
+        }
+        res.redirect('/Cliente');
+    });
+});
+
 //Rotas destinadas ao Funcionario
 
 app.get('/funcionario', (req, res) => {
-    res.render('Funcionario')
+    const query = 'SELECT * FROM functionary';
+    connection.query(query, (err, rows) => {
+        if (err) {
+            console.error('Erro ao inserir dados:', err);
+            res.status(500).send('Erro ao inserir dados no banco de dados.');
+            return;
+        }
+        console.log(rows);
+        res.render('Funcionario', { rows });
+    })
+
+});
+
+app.post('/funcionario', (req, res) => {
+    let name = req.body.name;
+    let cpf = req.body.cpf.replace(/[^\d]/g, '');
+    let address = req.body.address;
+    let phone = req.body.phone.replace(/[^\d]/g, '');
+    const query = 'INSERT INTO functionary (name, cpf, address, phone) VALUES (?, ?, ?, ?)';
+    connection.query(query, [name, cpf, address, phone], (err, results) => {
+        if (err) {
+            console.error('Erro ao inserir dados:', err);
+            res.status(500).send('Erro ao inserir dados no banco de dados.');
+            return;
+        }
+        res.redirect('/Funcionario');
+    });
+});
+
+app.get('/funcionario/edit/:id', (req, res) => {
+    console.log(req.params);
+    let id = req.params.id;
+    const query = "SELECT * FROM functionary WHERE id = ?";
+    connection.query(query, [id], (err, results) => {
+        if (err) {
+            console.error('Erro ao editar dados:', err);
+            res.status(500).send('Erro ao editar dados no banco de dados.');
+            return;
+        }
+
+        res.render('FuncionarioEdit', { functionary: results[0] });
+    });
+})
+
+app.post('/funcionario/update/:id', (req, res) => {
+    let id = req.params.id;
+    const { name, cpf, address, phone } = req.body;
+    const cleanCpf = cpf.replace(/[^\d]/g, '');
+    const cleanPhone = phone.replace(/[^\d]/g, '');
+
+    const query = "UPDATE functionary SET name = ?, cpf = ?, address = ?, phone = ? WHERE id = ?";
+
+    connection.query(query, [name, cleanCpf, address, cleanPhone, id], (err, results) => {
+        if (err) {
+            console.error('Erro ao atualizar dados:', err);
+            res.status(500).send('Erro ao atualizar dados no banco de dados.');
+            return;
+        }
+
+        res.redirect('/Funcionario');
+    });
+});
+
+app.post('/funcionario/delete/:id', (req, res) => {
+    let id = req.params.id;
+    const query = "DELETE FROM functionary WHERE id = ?";
+    connection.query(query, [id], (err, results) => {
+        if (err) {
+            console.error('Erro ao deletar dados:', err);
+            res.status(500).send('Erro ao deletar dados no banco de dados.');
+            return;
+        }
+        res.redirect('/Funcionario');
+    });
 });
 
 //Rotas destinadas a Venda
 
 app.get('/venda', (req, res) => {
-    res.render('Venda')
+    const customerQuery = 'SELECT * FROM customer';
+    const functionaryQuery = 'SELECT * FROM functionary';
+    const productQuery = 'SELECT * FROM product';
+    const saleQuerry = `SELECT 
+            sale.id AS sale_id,
+            sale.name AS sale_name,
+            customer.id AS customer_id,
+            customer.name AS customer_name,
+            functionary.id AS functionary_id,
+            functionary.name AS functionary_name,
+            product.id AS product_id,
+            product.name AS product_name
+        FROM 
+            sale
+        INNER JOIN 
+            customer ON sale.customer_id = customer.id
+        INNER JOIN 
+            functionary ON sale.functionary_id = functionary.id
+        INNER JOIN 
+            product ON sale.product_id = product.id;`;
+    connection.query(saleQuerry, (err, sales) => {
+        if (err) {
+            console.error('Erro ao buscar vendas:', err);
+            res.status(500).send('Erro ao buscar vendas no banco de dados.');
+            return;
+        }
+        connection.query(customerQuery, (err, customers) => {
+            if (err) {
+                console.error('Erro ao buscar clientes:', err);
+                res.status(500).send('Erro ao buscar clientes no banco de dados.');
+                return;
+            }
+            connection.query(functionaryQuery, (err, functionaries) => {
+                if (err) {
+                    console.error('Erro ao buscar funcionários:', err);
+                    res.status(500).send('Erro ao buscar funcionários no banco de dados.');
+                    return;
+                }
+                connection.query(productQuery, (err, products) => {
+                    if (err) {
+                        console.error('Erro ao buscar produtos:', err);
+                        res.status(500).send('Erro ao buscar produtos no banco de dados.');
+                        return;
+                    }
+                    res.render('Venda', { customers, functionaries, products, sales });
+                });
+            });
+        });
+    })
+
+});
+
+app.post('/venda', (req, res) => {
+    let name = req.body.name;
+    let id_functionary = req.body.functionary_id;
+    let id_customer = req.body.customer_id;
+    let id_product = req.body.product_id;
+    const query = 'INSERT INTO sale (name, functionary_id, customer_id, product_id) VALUES (?, ?, ?, ?)';
+    connection.query(query, [name, id_functionary, id_customer, id_product], (err, results) => {
+        if (err) {
+            console.error('Erro ao inserir dados:', err);
+            res.status(500).send('Erro ao inserir dados no banco de dados.');
+            return;
+        }
+        res.redirect('/venda');
+    });
+});
+
+app.post('/venda/delete/:id', (req, res) => {
+    let id = req.params.id;
+    const query = "DELETE FROM sale WHERE id = ?";
+    connection.query(query, [id], (err, results) => {
+        if (err) {
+            console.error('Erro ao deletar dados:', err);
+            res.status(500).send('Erro ao deletar dados no banco de dados.');
+            return;
+        }
+        res.redirect('/venda');
+    });
 });
 
 // Rotas destinadas ao Produto
@@ -97,7 +295,7 @@ app.get('/produto/edit/:id', (req, res) => {
             return;
         }
 
-        res.render('ProdutoEdit', { customer: results[0] });
+        res.render('ProdutoEdit', { product: results[0] });
     });
 });
 
